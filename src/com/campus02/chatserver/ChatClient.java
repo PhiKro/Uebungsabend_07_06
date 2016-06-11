04e5611de1ac13daf823949d1606f2ada72be2b7
Philipp <Philipp@172.31.35.29>
2016-06-11 11:34:21 +0200

    qwe

--- src/com/campus02/chatserver/ChatClient.java
+++ src/com/campus02/chatserver/ChatClient.java
@@ -1,5 +1,5 @@
 package com.campus02.chatserver;
-
+ // ToDO Comments 
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,7 +9,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ChatClient implements Runnable{
+public class ChatClient implements Runnable {
 
 	private BufferedReader reader;
 	private PrintWriter printWriter;
@@ -17,12 +17,9 @@ public class ChatClient implements Runnable{
 	private Socket client;
 	private String name;
 	private HashMap<String, ChatClient> userNames;
-	
-	
-	
+
 	public ChatClient(ArrayList<ChatClient> clients, Socket client, HashMap<String, ChatClient> userNames) {
-		try 
-		{
+		try {
 			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			printWriter = new PrintWriter(client.getOutputStream());
 		} catch (IOException e) {
@@ -43,129 +40,133 @@ public class ChatClient implements Runnable{
 		userNames.put(this.name, this);
 	}
 
-	
-	
-	
-	public void sendMessage(String message)
-	{
+	public void sendMessage(String message) {
 		printWriter.println(message);
-		logMessage(message);
 		printWriter.flush();
 	}
 
-
 	@Override
-	public void run() 
-	{
-		while (true)
-		{
+	public void run() {
+		while (client.isConnected() && !(client.isClosed())) {
 			String message = "";
 			try {
 				message = reader.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			String[] input1 = message.split(":");
-			if(this.getName() == null && !(input1[0].equals("<name>")))
-			{
-				printWriter.println("!Sie können ohne einen Nick nicht Chatten bitte vergeben Sie einen Namen!");
-				printWriter.flush();
-			}
-			else {
-			if (input1[0].equals("<name>")&& input1.length ==2){
-				if (userNames.containsKey(input1[1]))
-				{
-					printWriter.println("Nick bereits vergeben, bitte wählen Sie einen anderen!");
+
+				String[] input1 = message.split(":");
+				if (this.getName() == null && !(input1[0].equals("<name>"))) {
+					sendError(1);
+				} else {
+					if (input1[0].equals("<name>") && input1.length == 2) {
+						if (userNames.containsKey(input1[1])) {
+							sendError(3);
+						} else {
+							newUser(input1);
+						}
+					} else if (input1[0].equalsIgnoreCase("<msg>")) {
+						sendToAll(input1);
+					} else if (input1[0].equalsIgnoreCase("<msgto>") && input1.length == 3) {
+						sendToUser(input1);
+					} else if (input1[0].equalsIgnoreCase("<bye>")) {
+						close();
+						break;
+					} else
+						sendError(2);
+					printWriter.flush();
 				}
-				else{
-				newUser(input1);
-				}
-				}
-			else if (input1[0].equalsIgnoreCase("<msg>"))
-			{
-				sendToAll(input1);
+			}// ToDo Multicatch!!!!! 
+			catch (Exception e) {
+				// e.printStackTrace();
+				this.close();
 			}
-			else if (input1[0].equalsIgnoreCase("<msgto>") && input1.length == 3)
-			{
-				sendToUser(input1);
-				
-			}
-			else if (input1[0].equalsIgnoreCase("<bye>")){
-				close();
-				break;}
-			
-			else
-				printWriter.println("Wrong Command");
-				printWriter.flush();
 		}
+	}
+
+	private void sendError(int id) {
+
+		switch (id) {
+		case 1:
+			printWriter.println("!Sie können ohne einen Nick nicht Chatten bitte vergeben Sie einen Namen!");
+			printWriter.flush();
+			break;
+
+		case 2:
+			printWriter.println("Wrong Command");
+			printWriter.flush();
+			break;
+
+		case 3:
+			printWriter.println("Nick bereits vergeben, bitte wählen Sie einen anderen!");
+			printWriter.flush();
+			break;
+
+		default:
+			printWriter.println("Es ist ein Fehler: Unbekannter Fehler aufgetreten. Wenden Sie sich an den Helpdesk!");
+			printWriter.flush();
+			break;
 		}
+
 	}
 
 	private void sendToUser(String[] input1) {
-		if(userNames.containsKey(input1[1]))
-		userNames.get(input1[1]).sendMessage("Private Nachricht von "+this.getName() + ": " + input1[2]);
-		else{
+		if (userNames.containsKey(input1[1]))
+			userNames.get(input1[1]).sendMessage("Private Nachricht von " + this.getName() + ": " + input1[2]);
+		else {
 			printWriter.println("Gewuenschter User nicht vorhanden");
 			printWriter.flush();
-			}
-		//				
-//				for (ChatClient client : clients) 
-//				{
-//					if (client.getName().equalsIgnoreCase(input1[1]))
-//					client.sendMessage("Private Nachricht von "+this.getName() + ": " + input1[2]);
-//				}
+		}
+
+		/*
+		 * for (ChatClient client : clients) { if
+		 * (client.getName().equalsIgnoreCase(input1[1])) client.sendMessage(
+		 * "Private Nachricht von "+this.getName() + ": " + input1[2]); }
+		 */
 	}
 
 	private void sendToAll(String[] input1) {
-		for (ChatClient client : clients) 
-		{
-			client.sendMessage(this.getName()+ ": " + input1[1]);
+		for (ChatClient client : clients) {
+			if (!(client.getName().equals(this.getName())))
+				client.sendMessage(this.getName() + ": " + input1[1]);
+		}
+	}
+
+	private void sendToAll(String input1) {
+		logMessage(input1);
+		for (ChatClient client : clients) {
+			if (!(client.getName().equals(this.getName())))
+				client.sendMessage(input1);
 		}
 	}
 
 	private void newUser(String[] input1) {
 		this.setName(input1[1]);
-		for (ChatClient client : clients) 
-		{
-			client.sendMessage(this.getName()+ " hat sich verbunden");
-		}
+		sendToAll(this.getName() + " hat sich verbunden");
 	}
-	
-	
-	public void close()
-	{
-		for (ChatClient client : clients) 
-		{
-			client.sendMessage(this.getName()+ " hat sich abgemeldet");
-		}
-		
+
+	public void close() {
+		sendToAll(this.getName() + " hat sich abgemeldet");
+
 		try {
-			
-			printWriter.println("Good Bye");			
+			printWriter.println("Good Bye");
 			this.reader.close();
 			this.printWriter.flush();
 			this.printWriter.close();
 			this.client.close();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			clients.remove(this);
 		}
 
 	}
-	
-	public void logMessage(String message)
-	{
-		try (PrintWriter logWriter = new PrintWriter(new FileOutputStream("E:\\Java\\chat.log",true),true);){
+
+	public void logMessage(String message) {
+		try (PrintWriter logWriter = new PrintWriter(new FileOutputStream("E:\\Java\\chat.log", true), true);) {
 			logWriter.println(message);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-
 
 }
